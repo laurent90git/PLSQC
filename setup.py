6fe97d309ad31cb511d1e25641718b407b5c0732
laurent.francois <lfrancoi@polytechnique.edu>
2026-02-05 13:28:25 +0100

    fix packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PLSQC',
+    name='plsqc',
     version='1.0.0',
     url='https://github.com/laurent90git/PLSQC',
     author='Laurent François',
